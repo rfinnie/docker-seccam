39bd36a49f55a8a0c1541c99e0e9d19dda0f219e
Ryan Finnie <ryan@finnie.org>
2019-02-25 23:33:00 -0800

    Use user in container

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM ubuntu:bionic
 
-COPY init /init
 COPY build.sh /tmp/
-
 RUN /tmp/build.sh
 RUN rm -f /tmp/build.sh
+
+COPY init /init
 
 CMD ["/init"]
--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y --no-install-recommends ffmpeg
+apt-get install -y --no-install-recommends ffmpeg daemontools
 apt-get clean
+adduser --system --home /streams --no-create-home --quiet streams
+mkdir -p /streams
+chown streams:nogroup /streams
